9751ff6de4d0adeb5bb353fcaec8256390583298
narasimhanarsi666-ship-it <narasimhanarsi666@gmail.com>
2026-01-30 18:28:19 +0530

    Update checkout.py

--- Backend/routes/checkout.py
+++ Backend/routes/checkout.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify####
 
 from services.cart_service import CartService
 from services.inventory_service import InventoryService
@@ -71,4 +71,5 @@ def prepare_checkout(user_id):
             "total": cart["total"],
         }
     })
+
 
